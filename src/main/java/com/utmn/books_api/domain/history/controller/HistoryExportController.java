bf04097bfb33032e2785b45e6d52f560bc5ad3ba
yespeace <damir.schaymuhametow2015@yandex.ru>
2024-11-18 22:09:26 +0500

    update: reminders/export

---
+++ src/main/java/com/utmn/books_api/domain/history/controller/HistoryExportController.java
@@ -0,0 +1,39 @@
+package com.utmn.books_api.domain.history.controller;
+
+import com.utmn.books_api.domain.history.service.HistoryExcelService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@Tag(name = "History Export Controller")
+@RestController
+@RequestMapping("/history-export")
+@RequiredArgsConstructor
+public class HistoryExportController {
+
+    private final HistoryExcelService historyExcelService;
+
+    @GetMapping("/reminders")
+    public ResponseEntity<ByteArrayResource> exportReminders() throws IOException {
+
+        var book = historyExcelService.exportReminders();
+        var stream = new ByteArrayOutputStream();
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(new MediaType("application", "vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+        header.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=reminders.xlsx");
+        book.write(stream);
+        book.close();
+        return new ResponseEntity<>(new ByteArrayResource(stream.toByteArray()),
+                header, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/utmn/books_api/domain/history/service/HistoryExcelService.java
@@ -0,0 +1,43 @@
+package com.utmn.books_api.domain.history.service;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+
+@RequiredArgsConstructor
+@Service
+public class HistoryExcelService {
+
+    private final HistoryService service;
+
+    //todo ширина колонок
+    public Workbook exportReminders() {
+        var items = service.reminders(Pageable.unpaged());
+
+        Workbook book = new XSSFWorkbook();
+        Sheet sheet = book.createSheet("Задержанные книги");
+        var rowIndex = 0;
+        var row = sheet.createRow(rowIndex);
+        row.createCell(0).setCellValue("№");
+        row.createCell(1).setCellValue("Название книги");
+        row.createCell(2).setCellValue("Имя клиента");
+        row.createCell(3).setCellValue("Дата выдачи книги");
+        row.createCell(3).setCellValue("Дата планируемой сдачи книги");
+
+        var formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        for (var item : items.getContent()) {
+            row = sheet.createRow(++rowIndex);
+            row.createCell(0).setCellValue(item.getId());
+            row.createCell(1).setCellValue(item.getTitle());
+            row.createCell(2).setCellValue(item.getCustomerName());
+            row.createCell(3).setCellValue(item.getDateOfIssue());
+            row.createCell(3).setCellValue(item.getReturnDate().format(formatter));
+        }
+        return book;
+    }
+}
